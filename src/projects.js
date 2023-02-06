18bfc001c6e4b516ee6e1134f1c6c1e76ddfbf0b
Danesh Tolani <danesh.tolani@gmail.com>
2023-02-06 10:27:25 +0530

    added projects.js

--- src/projects.js
+++ src/projects.js
@@ -1,28 +1,42 @@
 export var projects = [
-    {
-      react: [
-        {
-            title: "Todo Application",
-            github: "https://github.com/danesh-tolani/todo-assignment-frontend",
-            liveProject: ""
-        },
-        {
-            title: "Movie Recommender Application",
-            github: "https://github.com/danesh-tolani/movie-recommendation-app",
-            liveProject: ""
-        },
-        {
-            title: "Disney Plus Clone",
-            github: "https://github.com/danesh-tolani/disneyplus-react-clone",
-            liveProject: ""
-        }
-      ],
-      tailwind: [
-        {
-            title: "Todo Application",
-            github: "https://github.com/danesh-tolani/todo-assignment-frontend",
-            liveProject: ""   
-        }
-      ]
-    },
-]
+  {
+    react: [
+      {
+        title: "Todo Application",
+        github: "https://github.com/danesh-tolani/todo-assignment-frontend",
+        liveProject: "https://todo-assignment-frontend.vercel.app/",
+        description: "A web application where a user can login to crete new Todos, edit them or delete them and can also search for a particular todo based on keywords.",
+        image: "https://res.cloudinary.com/dhxg6ou2x/image/upload/v1675586480/projects/Todo_app_fbbrq2.jpg",
+        tools: ["React Js", "Mongo DB", "Express Js", "Axios", "jsonwebtoken", "bcryptjs"],
+        hosted: "Vercel",
+      },
+      {
+        title: "Movie Recommender Application",
+        github: "https://github.com/danesh-tolani/movie-recommendation-app",
+        liveProject: "https://movie-recommender-app.netlify.app/",
+        description:
+          "A website built using React JS where a user gets movie recommendations based on genres. A user can add movies to a watchlist and can search for movies based on some keywords and year of release.",
+        image: "https://res.cloudinary.com/dhxg6ou2x/image/upload/v1675586480/projects/Movie_recommender_hxsxld.jpg",
+        tools: ["React Js", "Redux-Toolkit", "Material UI", "Axios"],
+        hosted: "Netlify",
+      },
+      {
+        title: "Disney Plus Clone",
+        github: "https://github.com/danesh-tolani/disneyplus-react-clone",
+        liveProject: "https://disneyplus-clone-9bfc9.firebaseapp.com/",
+        description:
+          "Disney Plus Clone built using React Js and firebase for database and authentication. A user gets movies on the homepage and can click on any movie to get redirected to that movie page.",
+        image: "https://res.cloudinary.com/dhxg6ou2x/image/upload/v1675586480/projects/Disney_clone_ouxpt3.jpg",
+        tools: ["React Js", "Firebase", "Redux"],
+        hosted: "Firebase",
+      },
+    ],
+    tailwind: [
+      {
+        title: "Shopify Clone",
+        github: "https://github.com/danesh-tolani/shopify-responsive",
+        liveProject: "https://shopify-responsive.netlify.app/",
+      },
+    ],
+  },
+];
